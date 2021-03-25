ca882803e209b14dcd60bed6c09602b54cf09db4
Brandon Fonnemann <branjosef@gmail.com>
2021-03-24 19:33:06 -0500

    continued build-out of navbar - float right cart and user material icons

--- src/components/NavBarComponents/UpperNav.js
+++ src/components/NavBarComponents/UpperNav.js
@@ -4,27 +4,28 @@ import logo from './boxxie.png'
 import '../../styles/upperNavBar.css'
 
 function UpperNav() {
-    return (
-        <div className='navbar'>
-            <Link to='/'>
-                {/* slogan = marking header */}
-                {/* <i className='material-icons black'>home</i> */}
-                <img src={logo} alt='boxxie' className="branding-header" />
-            </Link>
+  return (
+    <div className='navbar'>
+      <Link to='/'>
+        {/* slogan = marking header */}
+        {/* <i className='material-icons black'>home</i> */}
+        <img src={logo} alt='boxxie' className="branding-header" />
+      </Link>
 
-            <div className="nav-search">
-                <i className='material-icons'>search</i>
-                <input
-                    className="nav-search-input"
-                    type="text"
-                />
-            </div>
+      {/* <div className="nav-search">
+        <i className='material-icons search-icon'>search</i>
+        <input
+          className="nav-search-input"
+          type="text"
+        />
+      </div> */}
+      <div className="navbar-right">
+        <Link to='/checkout'><i className='material-icons nav-icon'>shopping_cart</i></Link>
+        <Link to='/login'><i className="material-icons nav-icon">person </i></Link>
+      </div>
 
-            <Link to='/checkout'><i className='material-icons nav-icon'>shopping_cart</i></Link>
-            <Link to='/login'><i className="material-icons nav-icon">person </i></Link>
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default UpperNav
